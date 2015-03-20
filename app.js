7efe39958443399b34452498aa8c175e5ce87f10
html5challenge <tzs518@gmail.com>
2015-03-21 00:29:33 +0800

    create cat and circles

--- app.js
+++ app.js
@@ -1,15 +1,94 @@
 /**
  * Created by tzs518 on 3/19/2015.
  */
+//var stage = new createjs.Stage("gameView");
+//var gameView = new createjs.Container();
+//stage.addChild(gameView);
+//
+//var s = new createjs.Shape();
+//s.graphics.beginFill("#FF0000");
+//s.graphics.drawCircle(50,50,25);
+//s.graphics.endFill();
+//gameView.addChild(s);
+//
+//createjs.Ticker.setFPS(30);
+//createjs.Ticker.addEventListener("tick", stage);
+
 var stage = new createjs.Stage("gameView");
-var gameView = new createjs.Container();
-stage.addChild(gameView);
-
-var s = new createjs.Shape();
-s.graphics.beginFill("#FF0000");
-s.graphics.drawCircle(50,50,25);
-s.graphics.endFill();
-gameView.addChild(s);
-
 createjs.Ticker.setFPS(30);
 createjs.Ticker.addEventListener("tick", stage);
+
+var gameView = new createjs.Container();
+gameView.x = 30;
+gameView.y = 30;
+stage.addChild(gameView);
+
+var circleArr = [[],[],[],[],[],[],[],[],[]];
+var currentCat;
+
+function circleClicked(event){
+    if(event.target.getCircleType() !=3 ){
+        event.target.setCircleType(2);
+    }
+    if(currentCat.indexX == 0 || currentCat.indexX == 8 || currentCat.indexY == 0 || currentCat.indexY == 8){
+        alert("游戏结束");
+    }
+    var leftCircle = circleArr[currentCat.indexX-1][currentCat.indexY]; //向左移动
+    var rightCircle = circleArr[currentCat.indexX+1][currentCat.indexY]; //向右移动
+    var lefttopCircle = circleArr[currentCat.indexX-1][currentCat.indexY-1]; //向左上移动
+    var righttopCircle = circleArr[currentCat.indexX][currentCat.indexY-1]; //向右上移动
+    var leftbottomCircle = circleArr[currentCat.indexX-1][currentCat.indexY+1]; //向左下移动
+    var rightbottomCircle = circleArr[currentCat.indexX][currentCat.indexY+1]; //向右下移动
+
+    if(leftCircle.getCircleType()==1){
+        leftCircle.setCircleType(3);
+        currentCat.setCircleType(1);
+        currentCat = leftCircle;
+    }else if(rightCircle.getCircleType()==1){
+        rightCircle.setCircleType(3);
+        currentCat.setCircleType(1);
+        currentCat = rightCircle;
+    }else if(lefttopCircle.getCircleType()==1){
+        lefttopCircle.setCircleType(3);
+        currentCat.setCircleType(1);
+        currentCat = lefttopCircle;
+    }else if(righttopCircle.getCircleType()==1){
+        righttopCircle.setCircleType(3);
+        currentCat.setCircleType(1);
+        currentCat = righttopCircle;
+    }
+    else if(leftbottomCircle.getCircleType()==1){
+        leftbottomCircle.setCircleType(3);
+        currentCat.setCircleType(1);
+        currentCat = leftbottomCircle;
+    }
+    else if(rightbottomCircle.getCircleType()==1){
+        righttopCircle.setCircleType(3);
+        currentCat.setCircleType(1);
+        currentCat = righttopCircle;
+    }
+    else{
+        alert("游戏结束");
+    }
+}
+
+function addCircles(){
+    for(var indexY=0; indexY<9; indexY++){
+        for(var indexX=0; indexX<9; indexX++){
+            var c = new Circle();
+            gameView.addChild(c);
+            circleArr[indexX][indexY] = c;
+            c.indexX = indexX;
+            c.indexY = indexY;
+            c.x = indexY%2?indexX*55+25:indexX*55;
+            c.y = indexY*55;
+
+            if(indexX==4&&indexY==4){
+                c.setCircleType(3);
+                currentCat = c;
+            }
+            c.addEventListener("click", circleClicked);
+        }
+    }
+}
+addCircles();
